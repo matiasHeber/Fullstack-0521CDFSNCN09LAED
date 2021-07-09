ee433a0e00a19a11db59efe4c9e108432757536f
matiasHeber <mheber@digitalhouse.com>
2021-07-09 11:45:31 -0300

    Repo al día

---
+++ modulo4/clase14/repaso-flexbox/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPath));
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/index.html'))
+})
+
+
+app.listen('3000', ()=>{
+    console.log('Servidor corriendo en el puerto 3000');
+})
